a2f5a3ba898fe45569befead77183cc81988d0af
Garth <244253+xgp@users.noreply.github.com>
2022-12-20 15:55:08 +0100

    updated vars

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,12 @@
-mkdir -p ./postgresql_data;
-chown -R 1001:1001 ./postgresql_data;
+#!/bin/bash
+
+#mkdir -p ./postgresql_data;
+#chown -R 1001:1001 ./postgresql_data;
+
+$nebula_ip=$(ifconfig nebula1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+echo $nebula_ip
+export NEBULA_IP=$nebula_ip
+cat .env
+
+
+
