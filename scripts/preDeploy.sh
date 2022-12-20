6b2c664c0271c780af510daa6216f6d7e56083f1
Garth <244253+xgp@users.noreply.github.com>
2022-12-20 22:07:38 +0100

    updated ip script

--- scripts/preDeploy.sh
+++ scripts/preDeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "predeploy"
 #nebula_ip=$((ifconfig nebula1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'))
 nebula_ip=$(/sbin/ip -o -4 addr list nebula1 | awk '{print $4}' | cut -d/ -f1)
 echo $nebula_ip 
