b13df3ed578d47e6a5b17d15fe9aba77a9d992de
Garth <244253+xgp@users.noreply.github.com>
2022-12-20 19:30:07 +0100

    added nebula to infinispan bind

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#mkdir -p ./postgresql_data;
-#chown -R 1001:1001 ./postgresql_data;
+mkdir -p ./keycloak_data;
+chown -R 1000:1000 ./keycloak_data;
 
-$nebula_ip=$(ifconfig nebula1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+nebula_ip=$((ifconfig nebula1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'))
 echo $nebula_ip
 export NEBULA_IP=$nebula_ip
-
-
-
