a6cc0f48c0dad32c4c487c84c31d62983e41ef58
Garth <244253+xgp@users.noreply.github.com>
2022-12-22 08:23:40 +0100

    cleanup

--- scripts/preBackup.sh
+++
@@ -1,2 +0,0 @@
-#save latest dump to disk
-redis-cli -h $APP_IP -p $APP_PORT -a $SOFTWARE_PASSWORD save
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,7 +2,3 @@
 
 mkdir -p ./keycloak_data;
 chown -R 1000:1000 ./keycloak_data;
-
-nebula_ip=$((ifconfig nebula1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'))
-echo $nebula_ip
-export NEBULA_IP=$nebula_ip
