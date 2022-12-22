8f29dd6ce02338ef9a2ffe25a996f157ae7bb513
Garth <244253+xgp@users.noreply.github.com>
2022-12-22 15:51:20 +0100

    added to env

--- scripts/preDeploy.sh
+++ scripts/preDeploy.sh
@@ -2,4 +2,5 @@
 
 nebula_ip=$(/sbin/ip -o -4 addr list nebula1 | awk '{print $4}' | cut -d/ -f1)
 echo "NEBULA_IP=$nebula_ip" > .nebula
+echo "NEBULA_IP=$nebula_ip" >> .env
 export NEBULA_IP=$nebula_ip
